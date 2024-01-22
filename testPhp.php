afc03347eebb3e7d14ef67da1f586cf0048a3413
DominikR02 <125642128+DominikR02@users.noreply.github.com>
2024-01-22 13:11:52 +0100

    Mein erstes PHP

---
+++ testPhp.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Mein Erstes PHP</title>
+</head>
+<body>
+<h1>Hier entsteht mein erstes PHP Document</h1>
+</body>
+</html>
